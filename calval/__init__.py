7126df86abe7deec1aa9dfb581c64d2b29597c46
javitausia <jtausiahoyal@gmail.com>
2021-08-24 12:09:32 +0200

    add calval python code instead of lib

--- calval/__init__.py
+++ calval/__init__.py
@@ -4,8 +4,8 @@ Module attrs
 
 __version__     = '1.0'
 __author__      = 'GeoOcean(UC)'
-__contact__     = 'jth160@alumnos.unican.es'
-__url__         = 'https://github.com/javitausia/CalVal/lib/'
+__contact__     = 'tausiaj@unican.es'
+__url__         = 'https://github.com/javitausia/CalVal/calval/'
 __description__ = 'Calibration-Validation of hindcast data'
 
 from . import calval
